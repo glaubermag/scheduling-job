1a5d397ce70a7a2fdcedd508b3f1e94e646630cf
Glauber Ariel Magalhães <glaubermagalhaes@outlook.com>
2020-05-19 09:38:55 -0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,3 @@
+var Job = [{ "ID": 1, "Description": "Importação de arquivos de fundos", "MaxDate": "2019-11-11 08:00:00", "TimeToDo": 2 }, { "ID": 2, "Description": "Importação de dados da Base Legada", "MaxDate": "2019-11-11 12:00:00", "TimeToDo": 4 }, { "ID": 3, "Description": "Importação de dados de integração", "MaxDate": "2019-11-11 08:00:00", "TimeToDo": 6 }]
+
+console.log(Job[0]);
